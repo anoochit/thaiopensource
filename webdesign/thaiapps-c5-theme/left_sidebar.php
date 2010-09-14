05b878da5bb4617ce578f2319a6cf1cd01e6117a
anoochit <anoochit@3eeee23f-a092-dd85-a815-e89897ed6760>
2010-09-14 09:25:11 +0000

    update webdesign

---
+++ webdesign/thaiapps-c5-theme/left_sidebar.php
@@ -0,0 +1,50 @@
+<html>
+<head>
+<?php Loader::element('header_required'); ?>
+<link rel="stylesheet" type="text/css" href="<?=$this->getThemePath();?>/styles.css" />
+</head>
+<body>
+
+<div id="wrap">
+	<div id="subheader">
+        <!-- menu -->
+        <?php
+               $a = new Area('Header Nav');
+               $a->display($c);
+        ?>
+    </div>
+	<div id="header"><img src="<?=$this->getThemePath();?>/images/logo.png" alt="Thai OS"></div>
+	<div id="nav2">
+        <!-- nav2 -->
+        <?php
+            $a = new Area('Header Content');
+            $a->display($c);
+        ?>
+	</div>
+	<div id="main">
+	  <div id="round" >
+            <!-- body message -->
+            <?php
+               $a = new Area('Main Content');
+               $a->display($c);
+            ?>
+		</div>		
+	</div>
+	<div id="sidebar">
+	    <div id="round" >
+            <?php
+               $a = new Area('Sidebar Content');
+               $a->display($c);
+            ?>	    
+        </div>				
+	</div>
+	<div id="footer">
+		<?
+           $a = new Area('Footer Content');
+            $a->display($c);
+        ?>
+
+	</div>
+</div> 
+</body>
+</html>
