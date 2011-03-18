633246e7b6516285921215b5d135ab3295762c7e
anoochit <anoochit@3eeee23f-a092-dd85-a815-e89897ed6760>
2011-03-18 02:23:42 +0000

    add toss theme for drupal

---
+++ webdesign/toss2011-drupal-theme/themes/toss2011/page-front.tpl.php
@@ -0,0 +1,81 @@
+<?php
+// $Id: page.tpl.php,v 1.1.2.1 2009/02/24 15:34:45 dvessel Exp $
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language ?>" lang="<?php print $language->language ?>" dir="<?php print $language->dir ?>">
+
+<head>
+  <title><?php print $head_title; ?></title>
+  <?php print $head; ?>
+  <?php print $styles; ?>
+  <?php print $scripts; ?>
+</head>
+
+<body class="<?php print $body_classes; ?> show-grid">
+  <div id="page" class="container-16 clear-block">
+ 
+    <div id="site-header" class="clear-block">
+
+    <?php if ($main_menu_links || $secondary_menu_links): ?>
+      <div id="site-menu" class="grid-16">
+        <?php print $main_menu_links; ?>
+        <?php print $secondary_menu_links; ?>
+      </div>
+    <?php endif; ?>
+
+    </div>
+
+
+    <div id="site-subheader" class="prefix-0 suffix-0 clear-block">
+    <?php if ($topheader): ?>
+      <?php print $topheader; ?>
+    <?php endif; ?>
+    <div id="logo"><img src="<?php print base_path().path_to_theme(); ?>/images/tosslogo.png" ></div>
+    <?php if ($header): ?>
+      <div id="header-region" class="region grid-16 clear-block">
+        <?php print $header; ?>
+      </div>
+    <?php endif; ?>
+    </div>
+
+
+    <div id="main" class="column grid-16 ">
+      <div id="main-content" class="region clear-block">
+        <!--<?php print $content; ?>-->
+      </div>
+
+      <?php print $feed_icons; ?>
+    </div>
+
+  <div id="area" class="grid-16">
+      <div id="area1" class="grid-6 alpha">
+         <?php print $area1; ?>
+      </div>
+      <div id="area2" class="grid-6">   
+         <?php print $area2; ?>
+      </div>
+      <div id="area3" class="grid-4 omega">   
+         <?php print $area3; ?>
+      </div>
+  </div>
+
+  <div id="footer" class="prefix-0 suffix-0">
+    <?php if ($footer): ?>
+      <div id="footer-region" class="region grid-16 clear-block">
+        <?php print $footer; ?>
+      </div>
+    <?php endif; ?>
+
+    <?php if ($footer_message): ?>
+      <div id="footer-message" class="grid-16">
+        <?php print $footer_message; ?>
+      </div>
+    <?php endif; ?>
+  </div>
+
+  </div>
+
+  <?php print $closure; ?>
+</body>
+</html>
