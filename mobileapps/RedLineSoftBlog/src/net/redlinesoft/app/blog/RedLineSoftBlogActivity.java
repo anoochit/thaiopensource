cf06bfca09a5408818e97af794f427f1a537fac6
anoochit <anoochit@3eeee23f-a092-dd85-a815-e89897ed6760>
2011-10-09 16:28:36 +0000

    add sample mobile apps and export key from redlinesoft

---
+++ mobileapps/RedLineSoftBlog/src/net/redlinesoft/app/blog/RedLineSoftBlogActivity.java
@@ -0,0 +1,99 @@
+package net.redlinesoft.app.blog;
+
+import android.app.Activity;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.os.Bundle;
+import android.view.Window;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+public class RedLineSoftBlogActivity extends Activity {
+    /** Called when the activity is first created. */
+	final Activity activity = this;
+	
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.getWindow().requestFeature(Window.FEATURE_PROGRESS);
+        setContentView(R.layout.main);
+        
+        if (!chkStatus()) {        
+        	Toast.makeText(this, "No Network " , Toast.LENGTH_LONG).show();
+        } else {
+                
+        
+        final WebView webView = (WebView) findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        
+        /**
+         * progress bar
+         */
+        webView.setWebChromeClient(new WebChromeClient() {
+            public void onProgressChanged(WebView view, int progress)
+            {
+                activity.setTitle("Loading...");
+                activity.setProgress(progress * 100);
+ 
+                if(progress == 100)
+                    activity.setTitle(R.string.app_name);
+            }
+        });
+        
+        /**
+         * show the same view 
+         */
+        webView.setWebViewClient(new WebViewClient() {
+        	@Override
+        	public void onReceivedError(WebView view, int errorCode, String description, String failingUrl)
+            {
+                // Handle the error
+        		webView.loadUrl("about:home");
+        		Toast.makeText(getApplicationContext(), description, Toast.LENGTH_LONG).show();        		
+            }
+ 
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url)
+            {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+        
+        /**
+         * load url
+         */
+        webView.loadUrl("http://blog.redlinesoft.net");
+        
+        }
+        
+    }
+    
+    public boolean chkStatus() {
+    	final ConnectivityManager connMgr = (ConnectivityManager)
+    	this.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+    	final android.net.NetworkInfo wifi =
+    	connMgr.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+
+    	final android.net.NetworkInfo mobile =
+    	connMgr.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+
+	    if( wifi.isAvailable() ){
+	    	//Toast.makeText(this, "Wifi" , Toast.LENGTH_LONG).show();
+	    	return true;
+	    } else if( mobile.isAvailable() ){
+	    	//Toast.makeText(this, "Mobile 3G " , Toast.LENGTH_LONG).show();
+	    	return true;
+	    } else {
+	    	Toast.makeText(this, "No Network " , Toast.LENGTH_LONG).show();
+	    	return false;	
+	    }
+				
+    }
+
+    
+    
+}
