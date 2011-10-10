a01c425abdd85553137fc7ec063554601d77daab
anoochit <anoochit@3eeee23f-a092-dd85-a815-e89897ed6760>
2011-10-10 09:39:32 +0000

    distable back button function

--- mobileapps/toss-mobile-app/src/net/redlinesoft/app/thaiopensource/ThaiOpenSourceActivity.java
+++ mobileapps/toss-mobile-app/src/net/redlinesoft/app/thaiopensource/ThaiOpenSourceActivity.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.os.Bundle;
-import android.view.KeyEvent;
 import android.view.Window;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
