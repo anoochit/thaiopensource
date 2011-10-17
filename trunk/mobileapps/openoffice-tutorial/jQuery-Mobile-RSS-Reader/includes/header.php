6a68a6154382a6c1455375b445c9c27326053dba
anoochit <anoochit@3eeee23f-a092-dd85-a815-e89897ed6760>
2011-10-17 16:01:18 +0000

    fix ubuntu release party app and add new OOo mobile web

---
+++ trunk/mobileapps/openoffice-tutorial/jQuery-Mobile-RSS-Reader/includes/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html> 
+<html> 
+   <head> 
+   <meta charset="utf-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+
+   <title><?php echo isset($siteName) ? ucWords($siteName) : 'Tuts+'; ?></title> 
+
+   <link rel="stylesheet" href="jquery-mobile/jquery.mobile-1.0rc1.min.css" />
+   <link rel="apple-touch-icon" href="img/tutsTouchIcon.jpg" />
+   <link rel="stylesheet" href="css/mobile.css" />
+   <link rel="stylesheet" href="css/article.css" />
+
+   <script src="jquery/jquery-1.6.4.min.js"></script>
+   <script src="jquery-mobile/jquery.mobile-1.0rc1.min.js"></script>
+</head> 
