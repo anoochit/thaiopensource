153aec4d160140e9108f7b13dbdc2cc5e9005664
anoochit@gmail.com <anoochit@gmail.com@3eeee23f-a092-dd85-a815-e89897ed6760>
2011-10-21 14:00:44 +0000

    add random number for fresh css

--- mobileapps/iampetdo/includes/header.php
+++ mobileapps/iampetdo/includes/header.php
@@ -8,7 +8,7 @@
 
    <link rel="stylesheet" href="jquery-mobile/jquery.mobile-1.0rc1.min.css" />
    <link rel="apple-touch-icon" href="img/icon.jpg" />
-   <link rel="stylesheet" href="css/mobile.css" />
+   <link rel="stylesheet" href="css/mobile.css<?=time();?>" />
 
    <script src="jquery/jquery-1.6.4.min.js"></script>
    <script src="jquery-mobile/jquery.mobile-1.0rc1.min.js"></script>
