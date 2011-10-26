8b5c428be358c142d44946cc729f6597c6c09cb1
anoochit <anoochit@3eeee23f-a092-dd85-a815-e89897ed6760>
2011-10-26 04:27:42 +0000

    fix toss mobile web cache

--- mobileapps/toss-mobile/site.php
+++ mobileapps/toss-mobile/site.php
@@ -15,14 +15,11 @@ if ( !in_array($siteName, $siteList) ) {
    $siteName = 'news';
 }
 
-$cache = dirname(__FILE__) . "/cache/$siteName";
+
+$cache = dirname(__FILE__) . "/cache/".$siteName;
 // Re-cache every three hours
-if(filemtime($cache) < (time() - 10800))
-{
-   // Get from server
-   if ( !file_exists(dirname(__FILE__) . '/cache') ) {
-      mkdir(dirname(__FILE__) . '/cache', 0777);
-   }
+if (((filemtime($cache)) < (time() - 10800)) OR (!file_exists($cache))) {
+
    // YQL query (SELECT * from feed ... ) // Split for readability
    $path = "http://query.yahooapis.com/v1/public/yql?q=";
    $path .= urlencode("SELECT * FROM feed WHERE url='http://thaiopensource.org/view/feed/$siteName'");
@@ -31,15 +28,15 @@ if(filemtime($cache) < (time() - 10800))
    // Call YQL, and if the query didn't fail, cache the returned data
    $feed = file_get_contents($path, true);
 
+   var_dump($feed);
    // If something was returned, cache
-   if ( is_object($feed) && $feed->query->count ) {
+   //if ( is_object($feed) && $feed->query->count ) {
       $cachefile = fopen($cache, 'wb');
       fwrite($cachefile, $feed);
       fclose($cachefile);
-   }
-}
-else
-{
+   //}
+   
+} else {
    // We already have local cache. Use that instead.
    $feed = file_get_contents($cache);
 }
--- mobileapps/toss-mobile/views/article.tmpl.php
+++ mobileapps/toss-mobile/views/article.tmpl.php
@@ -4,8 +4,8 @@
 <div data-role="page">
 
    <header data-role="header" class="<?php echo $siteName; ?>">
-      <a  data-rel="back" data-role="button" data-icon="back" >Back</a>
-      <h1> <?php echo $feed->title; ?> </h1>
+      <!--<a  data-rel="back" data-role="button" data-icon="back" >Back</a> -->
+      <h1> <?php echo ucwords($siteName); ?> </h1>
    </header><!-- /header -->
 
    <div data-role="content">	
@@ -13,9 +13,9 @@
         <div><?php echo str_replace("read more","",$feed->description); ?> </div>        
    </div><!-- /content -->
 
-   <footer data-role="footer" data-position="fixed">
+   <!-- <footer data-role="footer" data-position="fixed">
       <h4> &copy; <?=date("Y");?> thaiopensource.org</h4>
-   </footer>
+   </footer> -->
 </div>
 
 </body>
