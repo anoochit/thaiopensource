3b6266406ab3d61befa69a7fa37b88af16a4103b
anoochit <anoochit@3eeee23f-a092-dd85-a815-e89897ed6760>
2011-10-26 04:42:12 +0000

    remove mobile.css

--- mobileapps/toss-mobile/index.php
+++ mobileapps/toss-mobile/index.php
@@ -1,36 +1,36 @@
 <?php include('includes/header.php'); ?>
 <body>
- <div data-role="page">
+	<div data-role="page">
 
-   <header data-role="header" data-them="a">
-	<h1>TOSS Mob!le</h1>
-   </header>
+		<header data-role="header" data-them="a">
+			<h1>TOSS Mob!le</h1>
+		</header>
 
-   <div data-role="content">
-	<ul data-role="listview" data-theme="c" >  
-      <li>                     
-         <a href="site.php?siteName=news"> News </a>  
-      </li>
-      <li>                     
-         <a href="site.php?siteName=article"> Articles </a>  
-      </li>
-      <li>                     
-         <a href="site.php?siteName=blog"> Blog </a>  
-      </li>
-      <li>                     
-         <a href="site.php?siteName=review"> Review </a>  
-      </li>
-      <li>                     
-         <a href="site.php?siteName=howto"> How-To </a>  
-      </li>
-	</ul>
-   </div>
+		<div data-role="content">
+			<ul data-role="listview" data-theme="c">
+				<li> </li>
+				<li><a href="site.php?siteName=news"> News </a>
+				</li>
+				<li><a href="site.php?siteName=article"> Articles </a>
+				</li>
+				<li><a href="site.php?siteName=blog"> Blog </a>
+				</li>
+				<li><a href="site.php?siteName=review"> Review </a>
+				</li>
+				<li><a href="site.php?siteName=howto"> How-To </a>
+				</li>
+			</ul>
+		</div>
 
-   <footer data-role="footer" data-position="fixed">
-	<h4> &copy; <?=date("Y");?> thaiopensource.org</h4>
-   </footer>
+		<footer data-role="footer" data-position="fixed">
+			<h4>
+				&copy;
+				<?=date("Y");?>
+				thaiopensource.org
+			</h4>
+		</footer>
 
- </div>
+	</div>
 
 </body>
 </html>
